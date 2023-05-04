56bce1a867b29f6a5d2458bf5739486a6c36a383
ComputerScienceSky <78038241+ComputerScienceSky@users.noreply.github.com>
2023-05-04 15:11:50 -0400

    Created State Class and updated Tape class

--- State.java
+++ State.java
@@ -1,7 +1,50 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class State extends DaringDuckLab{
-    
+    private String name;
+    private boolean terminal;
+    private List<Transition> transitions = new ArrayList<>();
+    private char[] symbols = new char[0];
+
+    public State(String name, boolean terminal){
+        this.name = name;
+        this.terminal = terminal;
+    }
+
+    //symbol is the input in which the state takes this transition
+    public void addTransition(char symbol, Transition transition){
+        //adds the symbol to the end of the array
+        symbols = new char[symbols.length + 1];
+        symbols[symbols.length - 1] = symbol;
+
+        //adds transition to the end of the transition list
+        transitions.add(transition);
+
+    }
+
+    public Transition getTransition(char symbol){
+        for(int i = 0; i < symbols.length; i++){
+            if(symbols[i] == symbol){
+                return transitions.get(i);
+            }
+        }
+        return new Transition();
+    }
+
+    public boolean isTerminal(){
+        return terminal;
+    }
+
+    @Override
+    public String toString(){
+        return name;
+    }
 }
 
 // A State class that holds a name, various Transitions, and knows whether or not it is a terminal state.
-//perhaps this could access my statemachine class???
-//ask maria and matt to consult about this
+
+
+//State constructor with name and boolean is terminal state
+//add transition function that adds type transitions to a state
+//list of transitions
--- Tape.java
+++ Tape.java
@@ -1,52 +1,50 @@
 public class Tape extends DaringDuckLab{
     private char[] tape;
-    private int head;
+    private int pos;
 
     public Tape(String input) {
         this.tape = input.toCharArray();
-        this.head = 0;
+        this.pos = 0;
     }
 
     public void moveLeft() {
-        if (head == 0) {
+        //if the head is in the first position you need to increase the tape size to move left -- could move elements, but that would take rlly long
+        if (pos == 0) {
             char[] newTape = new char[tape.length * 2];
             System.arraycopy(tape, 0, newTape, tape.length, tape.length);
             tape = newTape;
-            head = tape.length / 2;
+            pos = tape.length / 2;
         } else {
-            head--;
+            pos--;
         }
     }
 
     public void moveRight() {
-        if (head == tape.length - 1) {
+        //if the head is in the last position you need to increase the size (pretty similar to moveLeft)
+        if (pos == tape.length - 1) {
             char[] newTape = new char[tape.length * 2];
             System.arraycopy(tape, 0, newTape, 0, tape.length);
             tape = newTape;
         }
-        head++;
+        //no else statement necessary cause you add to the pos either way
+        pos++;
     }
 
     public char read() {
-        return tape[head];
+        return tape[pos];
     }
 
     public void write(char symbol) {
-        tape[head] = symbol;
+        tape[pos] = symbol;
     }
 
-    public int countOnes() {
+    public String countOnes() {
         int count = 0;
-        for (char symbol : tape) {
-            if (symbol == '1') {
+        for(int i = 0; i < tape.length; i++){
+            if(tape[i] == 1){
                 count++;
             }
         }
-        return count;
-    }
-
-    @Override
-    public String toString() {
-        return new String(tape);
+        return "Number of Ones: " + count;
     }
 }
